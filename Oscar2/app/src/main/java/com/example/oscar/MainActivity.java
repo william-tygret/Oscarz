c949fbfb379820b576a5f0deaa35d7361edb136d
programmingnat <np7771@yahoo.com>
2016-02-29 12:44:56 -0500

    Added MainActivity

--- Oscar2/app/src/main/java/com/example/oscar/MainActivity.java
+++ Oscar2/app/src/main/java/com/example/oscar/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.oscar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView)findViewById(R.id.listView);
+
+        ArrayList<Actor>theBunchOfActors = new ArrayList<>();
+        Actor a = new Actor("LEO","70s",1);
+        theBunchOfActors.add(a);
+        ActorListCustomAdapter clca = new ActorListCustomAdapter(MainActivity.this,theBunchOfActors);
+
+        listView.setAdapter(clca);
+
     }
 }
