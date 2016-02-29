cc7ed3abea0ed062259a225bfa3f963e15f63261
William-Tygret <wctygret@gmail.com>
2016-02-29 11:47:45 -0600

    act list

--- Oscar2/app/src/main/java/com/example/oscar/MainActivity.java
+++ Oscar2/app/src/main/java/com/example/oscar/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.oscar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView mListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mListView = (ListView)findViewById(R.id.listView);
+
+        ArrayList<Actor> actingDudes = new ArrayList<Actor>();
+        Actor danny = new Actor("Danny McBride", "1971", 1000);
+        Actor tom = new Actor("Tom Cruise", "1975", 0);
+        Actor ed = new Actor("Ed Begley Jr.","1967", 0);
+
+        ActorListCustomAdapter actorListCustomAdapter = new ActorListCustomAdapter(MainActivity.this,actingDudes);
+
+        mListView.setAdapter(actorListCustomAdapter);
     }
 }
